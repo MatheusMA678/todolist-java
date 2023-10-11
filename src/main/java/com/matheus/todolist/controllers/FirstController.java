c98631bb1518743529b1bedad1fa340dff7a29a2
Matheus Mattos Avelino <matheusma2510@hotmail.com>
2023-10-11 19:22:16 +0000

    project initialized

---
+++ src/main/java/com/matheus/todolist/controllers/FirstController.java
@@ -0,0 +1,15 @@
+package com.matheus.todolist.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class FirstController {
+
+  @GetMapping()
+  public String firstMessage() {
+    return "Hello World!";
+  }
+}
